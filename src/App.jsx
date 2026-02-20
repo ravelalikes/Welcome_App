fe07221544e68d861348c4a43de02843886c6d01
ravelalikes <ravelalikes@kluniversity.in>
2026-02-20 10:59:50 +0530

    Initial welcome project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎉 Welcome to My React App</h1>
+      <p>This is my first deployed React project 🚀</p>
+    </div>
+  );
+}
+
+export default App;
